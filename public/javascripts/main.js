5c5da9194590d6510e469c2f80e0adc13e8cbe3f
adnaan <adnaan.velji@gmail.com>
2014-10-28 18:44:48 -0400

    Deleted html5shiv and jquery local js files (fetch from cdn); renamed files to be less stupid; cleaned up main.js and removed a lot of manual state switching for feedback

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,17 @@
 (function(exports){
 
+  // Create namespace for app-level variables
+  App = {};
+  App.offices = _offices;
+  App.officeId = _officeId;
+
   function Office(id, name, isPresent) {
     var self = this;
     self.name = ko.observable(name);
     self.id = ko.observable(id);
     self.isPresent = ko.observable(isPresent);
+    self.isConnected = ko.observable(false);
+    self.local
   }
 
   // Data model
@@ -12,8 +19,8 @@
     var self = this;
 
     self.offices = ko.observableArray();
-    for (var i = 0; i < _offices.length; i++) {
-      self.offices.push(new Office(_offices[i].id, _offices[i].name, _offices[i].present));
+    for (var i = 0; i < App.offices.length; i++) {
+      self.offices.push(new Office(App.offices[i].id, App.offices[i].name, App.offices[i].present));
     };
 
     self.changePresenceById = function(officeId, presence) {
@@ -24,7 +31,7 @@
     };
 
     self.callOffice = function(office) {
-      var call = peer.call(office.id(), window.localStream);
+      var call = peer.call(office.id(), App.localStream);
 
       // Wait for stream on the call, then set peer video display
       call.on('stream', function(stream){
@@ -32,13 +39,6 @@
       });
 
       call.on('close', state_idle);
-    };
-
-    self.changePresence1 = function() {
-      var firstoffice = ko.utils.arrayFirst(this.offices(), function(currentOffice) {
-        return currentOffice.id() == _officeId;
-      });
-      if (firstoffice) {firstoffice.isPresent(!firstoffice.isPresent());}
     };
   };
   var officesViewModel = new OfficesViewModel();
@@ -51,7 +51,7 @@
 
   // PeerJS object
   //aqu2ngp60qr7wrk9  // Personal API key; TODO: replace
-  var peer = new Peer(_officeId , { key: 'lwjd5qra8257b9', debug: 3, 
+  var peer = new Peer(App.officeId , { key: 'lwjd5qra8257b9', debug: 3, 
     config: {'iceServers': [
     { url: 'stun:stun.l.google.com:19302' } // Pass in optional STUN and TURN server for maximum network compatibility
   ]}});
@@ -63,66 +63,27 @@
   // Receiving a call
   peer.on('call', function(call){
     // Answer the call automatically
-    call.answer(window.localStream);
+    call.answer(App.localStream);
     state_call_in_progress(call);
   });
   peer.on('error', function(err){
     alert(err.message);
     // Return to idle state if error occurs
-    state_idle();
+    alert('Call receive failed')
   });
 
-  // Click handlers setup
+  // When everything read to start
   $(function(){
-
-    // Start call
-    $('#make-call').click(function(){
-      var call = peer.call($('#callto-id').val(), window.localStream);
-      state_call_in_progress(call);
-    });
-
-    // End call
-    $('#end-call').click(function(){
-      window.existingCall.close();
-      state_idle();
-    });
-
-    // Retry if getUserMedia fails
-    $('#state_setup-retry').click(function(){
-      $('#state_setup-error').hide();
-      state_setup();
-    });
-
-    // Get things started
-    state_setup();
-  });
-
-  function state_setup () {
-    $('#state_idle, #state_call_in_progress').hide();
-    $('#state_setup').show();
-    $('#state_setup-error').hide();
-
     // Get audio/video stream
     navigator.getUserMedia({audio: true, video: true}, function(stream){
       // Set your video displays
       $('#my-video').prop('src', URL.createObjectURL(stream));
+      App.localStream = stream;
+    }, function(){ alert('Cannot access camera/mic'); });
+  });
 
-      window.localStream = stream;
-      state_idle();
-    }, function(){ $('#state_setup-error').show(); });
-  }
-
-  function state_idle () {
-    $('#state_setup, #state_call_in_progress').hide();
-    $('#state_idle').show();
-  }
 
   function state_call_in_progress (call) {
-    // Hang up on an existing call if present
-    if (window.existingCall) {
-      window.existingCall.close();
-    }
-
     // Wait for stream on the call, then set peer video display
     call.on('stream', function(stream){
       $('#their-video').prop('src', URL.createObjectURL(stream));
@@ -132,7 +93,5 @@
     window.existingCall = call;
     $('#their-id').text(call.peer);
     call.on('close', state_idle);
-    $('#state_setup, #state_idle').hide();
-    $('#state_call_in_progress').show();
   }
 })(this);
